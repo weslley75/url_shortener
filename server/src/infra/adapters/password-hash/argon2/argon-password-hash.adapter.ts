7347ced11f9ddd48508029e86023a6875dece6db
Weslley Almeida <weslleyalmeida6@gmail.com>
2023-06-10 02:52:06 -0300

    fix: extends to implements

--- server/src/infra/adapters/password-hash/argon2/argon-password-hash.adapter.ts
+++ server/src/infra/adapters/password-hash/argon2/argon-password-hash.adapter.ts
@@ -1,7 +1,7 @@
 import { PasswordHash } from "../../../../domain/common/utils/password-hash/password-hash.interface";
 import argon2 from "argon2";
 
-export class ArgonPasswordHashAdapter extends PasswordHash {
+export class ArgonPasswordHashAdapter implements PasswordHash {
   async hash(password: string) {
     return argon2.hash(password);
   }
